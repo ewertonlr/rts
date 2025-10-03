f2774d530d31b205adc74b25cbd7e29dcc369532
Ewerton Rover <ewerton.rover@gmail.com>
2025-10-02 21:58:42 -0300

    Fix selection rectangle implementation in SelectionManager; update type and initialization

--- scripts/SelectionManager.cs
+++ scripts/SelectionManager.cs
@@ -5,7 +5,7 @@ public partial class SelectionManager : Control
 {
     private bool isDragging = false;
     private Vector2 startDraggingPosition;
-    private Rect2 selectionRect;
+    private ColorRect selectionRect;
 
     public override void _Input(InputEvent @event)
     {
@@ -15,6 +15,9 @@ public partial class SelectionManager : Control
             {
                 startDraggingPosition = GetGlobalMousePosition();
                 isDragging = true;
+                selectionRect = new ColorRect();
+                selectionRect.Color = new Color(Colors.Blue, 0.4f);
+                AddChild(selectionRect);
 
                 Log.Debug("Left mouse button clicked at position: " + startDraggingPosition);
 
@@ -22,6 +25,8 @@ public partial class SelectionManager : Control
             else if (mouseEvent.ButtonIndex == MouseButton.Left && !mouseEvent.Pressed)
             {
                 isDragging = false;
+                selectionRect?.QueueFree();
+                selectionRect = null;
 
                 Log.Debug("Left mouse button released at position: " + GetGlobalMousePosition());
 
@@ -32,7 +37,11 @@ public partial class SelectionManager : Control
             if (isDragging)
             {
                 Vector2 currentMousePosition = GetGlobalMousePosition();
-                selectionRect = new Rect2(startDraggingPosition, currentMousePosition - startDraggingPosition);
+
+                Vector2 minPos = startDraggingPosition.Min(currentMousePosition);
+
+                selectionRect.GlobalPosition = minPos;
+                selectionRect.Size = (currentMousePosition - startDraggingPosition).Abs();
 
                 Log.Debug("Dragging... Current selection rectangle: " + selectionRect);
                 // Update selection rectangle visual here
